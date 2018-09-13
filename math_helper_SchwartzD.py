27f0a9c1f7b9285f91c6c7f0e7ef258f5c44b43d
Devin Schwartz <devinschwartz9@gmail.com>
2018-09-13 13:36:58 -0500

    might have made quadratic work

--- math_helper_SchwartzD.py
+++ math_helper_SchwartzD.py
@@ -8,7 +8,14 @@ def quadratic(a,b,c):
 returns answer when given a, b, and c
 from formula ax^2+bx+c
     '''
-    if a!=0 and b**2-4*a*c>=0:
-        answer1=(sqrt(b**2-4*a*c)-b)/2*a
-        answer2=(sqrt(b**2-4*a*c)*-1-b)/2*a
+    if a==0:
+        return "Divide by zero error"
+    if b**2-4*a*c<0:
+        answer1=(sqrt(abs(b**2-4*a*c))-b)/(2*a)
+        answer2=(sqrt(abs(b**2-4*a*c))*-1-b)/(2*a)
+        return str(answer1)+'*i'
+    if b**2-4*a*c>=0:
+        answer1=(sqrt(b**2-4*a*c)-b)/(2*a)
+        answer2=(sqrt(b**2-4*a*c)*-1-b)/(2*a)
     return answer1
+print(quadratic(6,-7,-3))
