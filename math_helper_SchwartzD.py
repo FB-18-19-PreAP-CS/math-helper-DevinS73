d8785040369b3823572b1ef619157c88197f4f98
Devin Schwartz <devinschwartz9@gmail.com>
2018-09-18 14:10:49 -0500

    added pythagorean and started compound interest

--- math_helper_SchwartzD.py
+++ math_helper_SchwartzD.py
@@ -11,11 +11,9 @@ def quadratic(a,b,c):
     >>> quadratic(9,9,9)
     '''
     if a==0:
-        return "Divide by zero error"
+        return "Error: Cannot divide by zero"
     if b**2-4*a*c<0:
-        answer1=(sqrt(abs(b**2-4*a*c))-b)/(2*a)
-        answer2=(sqrt(abs(b**2-4*a*c))*-1-b)/(2*a)
-        return str(round(answer1,4))+'*i',str(round(answer2,4))+'*i'
+        return "Error: Cannot take the square root of a negative number"
     if b**2-4*a*c==0:
         answer1=(-b)/(2*a)
         return round(answer1,4)
@@ -24,4 +22,11 @@ def quadratic(a,b,c):
         answer2=(sqrt(b**2-4*a*c)*-1-b)/(2*a)
         answer1=round(answer1,4)
         return round(answer1,4), round(answer2,4)
+def distance(x,y,x1,y1):
+    x_squaredist=(x-x1)**2
+    y_squaredist=(y-y1)**2
+    dist=sqrt(x_squaredist+y_squaredist)
+    return dist
+def comp_interest(initial_amount,percent_interest,comp_per_year,years):
+    return
 print(quadratic(-6,-7,-3))
